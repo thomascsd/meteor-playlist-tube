94e239b3719e6af2c94a17f90191e742a9e35715
Thomas chang <thomascsd808@gmail.com>
2016-03-10 10:43:49 +0000

    add exsiing files from PlaylistTube

---
+++ app/client/lib/app.js
@@ -0,0 +1,4 @@
+'use strict';
+'global angular';
+
+let app = angular.module('tubeApp', []);
---
+++ app/client/services/userDataFactory.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const app = angular.module('tubeApp');
+
+/** 記錄User的資料 */
+app.factory('userDataFactory', ['$localStorage', function($localStorage) {
+    var userData = {
+        currentPlaylistID: '',
+        currentVideo: {},
+
+        /** Save token data */
+        token: function(token) {
+            if (token) {
+                $localStorage.token = token;
+            }
+            else {
+                return $localStorage.token;
+            }
+        },
+
+        /** Manage playlist */
+        list: {
+            add: function(item) {
+                var index = 0;
+                var added = false;
+
+                if (!$localStorage.list) {
+                    $localStorage.list = [];
+                }
+
+                index = _.findIndex($localStorage.list, function(o) {
+                    return o.id === item.id;
+                });
+                added = index !== -1;
+
+                if (!added) {
+                    $localStorage.list.push(item);
+                }
+
+                return added;
+            },
+            getItems: function() {
+                return $localStorage.list;
+            },
+            deleteItems: function() {
+                delete $localStorage.list;
+            }
+        }
+    };
+
+    return userData;
+}]);
