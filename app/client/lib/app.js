1807b7df5b09208a719ea2999e7f5d9eb36bbf6b
Thomas chang <thomascsd808@gmail.com>
2016-04-08 10:55:25 +0000

    add theme

--- app/client/lib/app.js
+++ app/client/lib/app.js
@@ -2,3 +2,9 @@
 'global angular';
 
 let app = angular.module('tubeApp', ['angular-meteor', 'ngMaterial', 'ngStorage', 'infinite-scroll', 'heightAutoResizer']);
+
+app.config(['$mdThemingProvider', function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('purple')
+        .accentPalette('brown');
+}]);
